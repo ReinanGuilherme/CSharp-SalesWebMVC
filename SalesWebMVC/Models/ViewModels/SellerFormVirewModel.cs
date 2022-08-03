386435169d86db010b4ab634d80aa10530f15f57
ReinanOliveira <reinan.oliveira@atoscapital.com.br>
2022-08-03 17:33:40 -0300

    SellerFormViewModel and Department select component

---
+++ SalesWebMVC/Models/ViewModels/SellerFormVirewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SalesWebMVC.Models.ViewModels
+{
+    public class SellerFormVirewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
